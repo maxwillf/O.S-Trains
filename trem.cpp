56c3a8db687356278f5038917edf0e15cf6cf37f
maxwillf <maxwilliam780@gmail.com>
2021-03-28 13:21:20 -0300

    initial version

---
+++ trem.cpp
@@ -0,0 +1,62 @@
+#include "trem.h"
+#include <QtCore>
+
+//Construtor
+Trem::Trem(int ID, int x, int y){
+    this->ID = ID;
+    this->x = x;
+    this->y = y;
+    velocidade = 100;
+}
+
+void Trem::mudarVelocidade(int velocidade){
+    this->velocidade = velocidade;
+}
+
+//Função a ser executada após executar trem->START
+void Trem::run(){
+    while(true){
+        switch(ID){
+        case 1:     //Trem 1
+            if (y == 30 && x <330)
+                x+=10;
+            else if (x == 330 && y < 150)
+                y+=10;
+            else if (x > 60 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 2: //Trem 2
+            if (y == 30 && x <600)
+                x+=10;
+            else if (x == 600 && y < 150)
+                y+=10;
+            else if (x > 330 && y == 150)
+                x-=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        case 3: //Trem 3
+            if (y == 290 && x > 200)
+                x-=10;
+            else if (x < 470 && y == 150)
+                x+=10;
+            else if (x == 470 && y <= 290)
+                y+=10;
+            else
+                y-=10;
+            emit updateGUI(ID, x,y);    //Emite um sinal
+            break;
+        default:
+            break;
+        }
+        msleep(velocidade);
+    }
+}
+
+
+
+
